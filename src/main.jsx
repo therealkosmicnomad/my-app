f7abacc2e267f92c4f5dd5840199e269fbcc3fdf
therealkosmicnomad <therealkosmicnomad@gmail.com>
2025-11-04 11:10:49 -0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import Editor from './Editor'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/editor" element={<Editor />} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/editor" element={<Editor />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
