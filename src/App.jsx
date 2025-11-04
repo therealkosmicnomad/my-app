13c04e999a0342ec6ec9d9af9de1d48ce98a6842
therealkosmicnomad <therealkosmicnomad@gmail.com>
2025-11-04 11:06:27 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// src/App.jsx
 import { useEffect } from 'react'
 import { supabase } from './supabaseClient'
 import { useNavigate } from 'react-router-dom'
@@ -10,24 +9,18 @@ export default function App() {
     supabase.auth.getSession().then(({ data: { session } }) => {
       if (session) navigate('/editor')
     })
-
-    const { data: listener } = supabase.auth.onAuthStateChange((_event, session) => {
-      if (session) navigate('/editor')
-    })
-
-    return () => listener.subscription.unsubscribe()
   }, [navigate])
 
   return (
-    <div style={{ 
-      backgroundColor: '#0892D0', 
-      height: '100vh', 
+    <div style={{
+      backgroundColor: '#0892D0',
+      height: '100vh',
       margin: 0,
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'center'
     }}>
-      {/* Login form will go here */}
+      {/* Login form goes here */}
     </div>
   )
 }
